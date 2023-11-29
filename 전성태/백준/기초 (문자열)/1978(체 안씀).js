fb22faba474ed62c75baf6314b0c0ee975512430
poqop721 <poqop721@naver.com>
2023-11-29 21:14:45 +0900

    [백준-1978] 소수 찾기

--- 전성태/백준/기초 (문자열)/1978(체 안씀).js
+++ 전성태/백준/기초 (문자열)/1978(체 안씀).js
@@ -3,7 +3,6 @@ const nums = input[0]
 const numArr = input[1].split(' ')
 
 let count = 0;
-//에라토스테네스의 체 사용
 numArr.forEach((num)=>{
     if(num !== 1){
         let isPrime = true;
@@ -23,4 +22,3 @@ console.log(count)
 
 // 시간 : 164Ms
 // 메모리 : 9344KB
-
