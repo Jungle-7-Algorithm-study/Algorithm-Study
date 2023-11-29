096607ce51b2186332a53104345067a3d48a1693
uuuuj <yjth4436@gmail.com>
2023-11-29 23:51:10 +0900

    [백준-11728]배열합치기 재시도

--- 이유정/백준/sort/11728.py
+++ 이유정/백준/sort/11728.py
@@ -1,10 +1,10 @@
 import sys
+
 N, M = map(int, sys.stdin.readline().split())
 
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
 C = A + B
-# answer = ' '.join(map(str, sorted(C)))
 C.sort()
 
-print(C)
+print(' '.join(map(str, C)))
