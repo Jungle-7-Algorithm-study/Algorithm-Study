a64713271f2a2f38d9d9f307d68391cf81500a19
tomoyo519 <vanessa.cheong1@gmail.com>
2023-11-30 19:47:12 +0900

    [백준] 1406

--- 정다희/백준/정렬/11728.py
+++ 정다희/백준/정렬/11728.py
@@ -2,12 +2,8 @@ import sys
 input = sys.stdin.readline;
 lenA, lenB = map(int, input().split());
 
-arrA = (list(map(int, input().split())))
-arrA.sort()
-arrB = (list(map(int, input().split())))
-arrB.sort();
-result = (set(arrA) | set(arrB));
-result = list(result)
-result.sort()
-# print(list(result));
-print(' '. join(map(str, result)))
+arrA = list(map(int, input().split()));
+arrB = list(map(int, input().split()))
+result = arrA + arrB
+result.sort();
+print(" ".join(map(str, result)))
