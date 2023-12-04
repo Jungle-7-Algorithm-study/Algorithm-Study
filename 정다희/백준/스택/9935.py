dd18bd0dc41f634fdce5d5c20f86d336a9315f8d
tomoyo519 <vanessa.cheong1@gmail.com>
2023-12-04 19:17:47 +0900

    [백준-9935] 문자열폭팔

--- 정다희/백준/스택/9935.py
+++ 정다희/백준/스택/9935.py
@@ -3,9 +3,19 @@ input = sys.stdin.readline
 
 words = input().strip()
 exp = input().strip();
-while(exp in words):
-    words = words.replace(exp, '');
-if words == "" :
-    print('FRULA');
-else:
-    print(words)
+
+len_bomb = len(exp)
+ans = []
+
+for i in words:
+    ans.append(i);
+    if("".join(ans[-len_bomb:]) == exp):
+        del ans[-len_bomb:]
+print("".join(ans) if ans else 'FRULA');
+
+# while(exp in words):
+#     words = words.replace(exp, '');
+# if words == "" :
+#     print('FRULA');
+# else:
+#     print(words)
