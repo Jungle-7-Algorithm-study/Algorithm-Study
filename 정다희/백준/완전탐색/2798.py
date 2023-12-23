70c5765859de1993d71a39f47ba95bcd1835af54
tomoyo519 <vanessa.cheong1@gmail.com>
2023-12-23 22:42:39 +0900

    [백준-2798] 블랙잭

--- 정다희/백준/완전탐색/2798.py
+++ 정다희/백준/완전탐색/2798.py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-cards, sum = map(int, input().split())
-array = list(map(int, input().split()));
-numList = [];
-for i in range(cards):
-    for j in range(i+1, cards):
-        for k in range(j+1, cards):
-            three = array[i]+ array[j]+ array[k]
-            if three > sum:
-                continue;
-            else:
-                numList.append(three);
-print(max(numList));
+from itertools import combinations
+
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+nlst = []
+
+for three in combinations(lst, 3):
+    if sum(three) > M:
+        continue
+    else:
+        nlst.append(sum(three))
+print(max(nlst))
