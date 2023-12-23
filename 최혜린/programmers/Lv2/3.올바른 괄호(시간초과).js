1eab6bc1ae034a052fa378601b87e355d92f0970
choihyerln <choi.hyehyehihi@gmail.com>
2023-12-23 14:53:42 +0900

    [programmers] 올바른 괄호

---
+++ 최혜린/programmers/Lv2/3.올바른 괄호(시간초과).js
@@ -0,0 +1,14 @@
+// 시간초과
+function solution(s) {
+    let stackCnt = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        stackCnt += s[i] === '(' ? 1 : -1;
+        if (stackCnt < 0)
+            return false;
+    }
+    return stackCnt === 0 ? true : false;
+}
+
+s = "()()"
+console.log(solution(s));
