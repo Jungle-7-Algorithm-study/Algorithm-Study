b62a8bad1ce13c30435d3ab47b886bb141184474
wltjd1688 <wltjd1688@gmail.com>
2023-12-23 23:44:26 +0900

    [프로그래머스-178871] 달리기 경주

--- 김지성/프로그래머스/LV1/250137.js
+++ 김지성/프로그래머스/LV1/250137.js
@@ -1,29 +1,29 @@
 function solution(bandage, health, attacks) {
-  const [t, x, y] = bandage;
-  let currentHealth = health;
-  let continuousTime = 0;
-  let attackIndex = 0;
-  let currentTime = 0;
+    const [t, x, y] = bandage;
+    let currentHealth = health;
+    let continuousTime = 0;
+    let attackIndex = 0;
+    let currentTime = 0;
 
-  while (currentTime <= attacks[attacks.length - 1][0] + t) {
-      if (attackIndex < attacks.length && attacks[attackIndex][0] === currentTime) {
-          currentHealth -= attacks[attackIndex][1];
-          attackIndex++;
-          continuousTime = 0;
-      } else {
-          if (continuousTime < t) {
-              currentHealth = Math.min(currentHealth + x, health);
-              continuousTime++;
-          }
-          if (continuousTime === t) {
-              currentHealth = Math.min(currentHealth + y, health);
-              continuousTime = 0;
-          }
-      }
-      if (currentHealth <= 0) {
-          return -1;
-      }
-      currentTime++;
-  }
-  return currentHealth;
+    while (currentTime <= attacks[attacks.length - 1][0] + t) {
+        if (attackIndex < attacks.length && attacks[attackIndex][0] === currentTime) {
+            currentHealth -= attacks[attackIndex][1];
+            attackIndex++;
+            continuousTime = 0;
+        } else {
+            if (continuousTime < t) {
+                currentHealth = Math.min(currentHealth + x, health);
+                continuousTime++;
+            }
+            if (continuousTime === t) {
+                currentHealth = Math.min(currentHealth + y, health);
+                continuousTime = 0;
+            }
+        }
+        if (currentHealth <= 0) {
+            return -1;
+        }
+        currentTime++;
+    }
+    return currentHealth;
 }
