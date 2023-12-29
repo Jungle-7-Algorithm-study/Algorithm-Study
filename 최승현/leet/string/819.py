7b8027da2861b09ad51c268dcc0bf8f35ca51f26
ChoiWheatley <chltmdgus604@naver.com>
2023-12-29 13:35:01 +0000

    [leet-819] 🆗 most-common-word
    별도의 split 함수를 사용

--- 최승현/leet/string/819.py
+++ 최승현/leet/string/819.py
@@ -4,7 +4,7 @@ https://leetcode.com/problems/most-common-word/
 
 
 import re
-from typing import List
+from typing import Generator, List
 from collections import Counter
 
 
@@ -26,4 +26,21 @@ class Solution2:
             if w not in banned
         ]
         counter = Counter(words)
+        return counter.most_common()[0][0]
+
+
+class Solution3:
+    def getSplit(self, s: str):
+        word = ""
+        for i in s.lower():
+            if i not in "!?',;. ":
+                word += i
+            elif word:
+                yield word
+                word = ""
+        if word:
+            yield word
+
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        counter = Counter(w for w in self.getSplit(paragraph) if w not in banned)
         return counter.most_common()[0][0]
