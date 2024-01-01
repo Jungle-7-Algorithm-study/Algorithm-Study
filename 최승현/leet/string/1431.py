810bd7e0a4741f0f383d83cbdceb8d2e81f10570
ChoiWheatley <chltmdgus604@naver.com>
2024-01-01 14:42:01 +0000

    [leet-1431] 🆗 kids with the greatest number of candies

--- 최승현/leet/string/1431.py
+++ 최승현/leet/string/1431.py
@@ -8,7 +8,8 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         maximum = max(candies)
-        return [False if x + extraCandies < maximum else True for x in candies]
+        return [x + extraCandies >= maximum for x in candies]
+
 
 
 if __name__ == "__main__":
