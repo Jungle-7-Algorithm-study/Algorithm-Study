c1b7b23a2d14ecb5d3382d7866bf9a49dd23f988
choihyerln <choi.hyehyehihi@gmail.com>
2024-01-11 16:43:00 +0900

    [programmers] N개의 최소공배수

---
+++ 최혜린/programmers/Lv2/11.N개의 최소공배수.js
@@ -0,0 +1,9 @@
+// 최대공약수 구하는 함수: 유클리드 호제법
+function gcd(a,b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+}
+
+function solution(arr) {
+  return arr.reduce((a, b) => (a * b) / gcd(a,b));
+}
