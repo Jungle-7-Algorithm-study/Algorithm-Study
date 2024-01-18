c362e826fde4850bd822e723bc895b0e641b5069
poqop721 <poqop721@naver.com>
2024-01-18 21:54:56 +0900

    [백준-1920] 수 찾기_

--- 전성태/백준/이분 탐색/1920.js
+++ 전성태/백준/이분 탐색/1920.js
@@ -1,49 +1,18 @@
-// function recur(start, end, search){
-//     if(start === end){
-//         if(A[start] === search)
-//             res.push(1)
-//         else   
-//             res.push(0)
-//         return
-//     }
-
-//     let middle = ~~((start + end)/2)
-//     if(A[middle] >= search)
-//         recur(start, middle, search)
-//     else
-//         recur(middle + 1, end, search)
-// }
-
-// const stdin = require('fs').readFileSync('/dev/stdin').toString().split('\n')
-// const N = parseInt(stdin[0])
-// const A = stdin[1].split(' ').map(Number)
-// const M = parseInt(stdin[2])
-// const Nums = stdin[3].split(' ').map(Number)
-// A.sort((a,b)=>a-b)
-// const res = []
-// for(let i = 0; i < M; i++){
-//     recur(0,N-1,Nums[i])
-// }
-
-// console.log(res.join('\n'))
-
-
+//방법 1
 function recur(start, end, search){
-    if(start > end){
-        res.push(0)
+    if(start === end){
+        if(A[start] === search)
+            res.push(1)
+        else   
+            res.push(0)
         return
     }
 
     let middle = ~~((start + end)/2)
-    if(A[middle] === search){
-        res.push(1)
-        return
-    }
-    else if(A[middle] > search){
-        recur(start, middle - 1, search)
-    } else {
+    if(A[middle] >= search)
+        recur(start, middle, search)
+    else
         recur(middle + 1, end, search)
-    }
 }
 
 const stdin = require('fs').readFileSync('/dev/stdin').toString().split('\n')
@@ -58,3 +27,36 @@ for(let i = 0; i < M; i++){
 }
 
 console.log(res.join('\n'))
+
+
+//방법 2
+// function recur(start, end, search){
+//     if(start > end){
+//         res.push(0)
+//         return
+//     }
+
+//     let middle = ~~((start + end)/2)
+//     if(A[middle] === search){
+//         res.push(1)
+//         return
+//     }
+//     else if(A[middle] > search){
+//         recur(start, middle - 1, search)
+//     } else {
+//         recur(middle + 1, end, search)
+//     }
+// }
+
+// const stdin = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+// const N = parseInt(stdin[0])
+// const A = stdin[1].split(' ').map(Number)
+// const M = parseInt(stdin[2])
+// const Nums = stdin[3].split(' ').map(Number)
+// A.sort((a,b)=>a-b)
+// const res = []
+// for(let i = 0; i < M; i++){
+//     recur(0,N-1,Nums[i])
+// }
+
+// console.log(res.join('\n'))
