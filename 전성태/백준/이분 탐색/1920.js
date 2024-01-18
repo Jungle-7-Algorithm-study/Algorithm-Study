d61063dfb8ade4326b5f1a03f9b08ede4e04f988
poqop721 <poqop721@naver.com>
2024-01-18 21:35:35 +0900

    [백준-1920] 수 찾기

---
+++ 전성태/백준/이분 탐색/1920.js
@@ -0,0 +1,54 @@
+// function recur(start, end, search){
+//     if(start == end){
+//         if(A[start] === search)
+//             console.log(1)
+//         else   
+//             console.log(0)
+//         return
+//     }
+
+//     let middle = ~~((start + end)/2)
+//     if(A[middle] >= search)
+//         recur(start, middle, search)
+//     else
+//         recur(middle + 1, end, search)
+// }
+
+// const stdin = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+// const N = parseInt(stdin[0])
+// const A = stdin[1].split(' ').map(Number)
+// const M = parseInt(stdin[2])
+// const Nums = stdin[3].split(' ').map(Number)
+// A.sort((a,b)=>a-b)
+// for(let i = 0; i < M; i++){
+//     recur(0,N-1,Nums[i])
+// }
+
+function recur(start, end, search){
+    if(start == end){
+        if(A[start] === search)
+            res.push(1)
+        else   
+            res.push(0)
+        return
+    }
+
+    let middle = ~~((start + end)/2)
+    if(A[middle] >= search)
+        recur(start, middle, search)
+    else
+        recur(middle + 1, end, search)
+}
+
+const stdin = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+const N = parseInt(stdin[0])
+const A = stdin[1].split(' ').map(Number)
+const M = parseInt(stdin[2])
+const Nums = stdin[3].split(' ').map(Number)
+A.sort((a,b)=>a-b)
+const res = []
+for(let i = 0; i < M; i++){
+    recur(0,N-1,Nums[i])
+}
+
+console.log(res.join('\n'))
