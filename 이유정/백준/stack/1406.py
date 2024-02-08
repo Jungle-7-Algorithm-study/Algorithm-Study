dd742d491a11fc627e25752ba885dd43224dfdbb
uuuuj <yjth4436@gmail.com>
2024-02-08 21:24:56 +0900

    [20240208]에디터

--- 이유정/백준/stack/1406.py
+++ 이유정/백준/stack/1406.py
@@ -1,21 +1,25 @@
 import sys
 
-st1 = list(sys.stdin.readline().rstrip())
-st2 = []
+left = list(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline())
 
-for _ in range(int(sys.stdin.readline())):
-    command = list(sys.stdin.readline().split())
+right = []
+#right 배열은 마지막에 reverse해서 문자열 붙이기
+for _ in range(n):
+    command = list(sys.stdin.readline().rstrip())
     if command[0] == 'L':
-        if st1:
-            st2.append(st1.pop())
-    elif command[0] == 'D':
-        if st2:
-            st1.append(st2.pop())
-    elif command[0] == 'B':
-        if st1:
-            st1.pop()
-    else:
-        st1.append(command[1])
+        if left:
+            right.append(left.pop()) #left의 마지막 원소를 pop해서 right의 첫번째 위치에 삽입.
 
-st1.extend(reversed(st2))
-print(''.join(st1))
+    elif command[0] == 'D': #커서를 오른쪽으로 한칸 이동. 커서가 문장의 맨뒤인 경우 무시.
+        if right: #커서가 문장의 맨 뒤인 경우, right 스택이 비어있다.
+            #right의 0번째 원소를 pop해서 left의 마지막에 삽입
+            left.append(right.pop())
+    elif command[0] == 'B': #커서 왼쪽 문자 삭제
+        #만약 커서가 문장의 가장 앞이라면 무시
+        if left:
+            left.pop()
+    else:
+        left.append(command[-1])
+left.extend(reversed(right))
+print(''.join(left))
