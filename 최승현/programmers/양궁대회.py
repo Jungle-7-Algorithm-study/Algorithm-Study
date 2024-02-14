af48a49a68e6f03aa76bacba3fb676459397f2ee
ChoiWheatley <chltmdgus604@naver.com>
2024-02-14 21:38:52 +0900

    [프로그래머스-양궁대회] 10 ** 10 이 아니라 2 ** 10 경우의 수로 풀어야 함

--- 최승현/programmers/양궁대회.py
+++ 최승현/programmers/양궁대회.py
@@ -12,6 +12,37 @@ def dfs(n: int, res: List[int], fn: Callable[[List[int]], None]) -> None:
         res[i] -= 1
 
 
+def dfs2(
+    arrow: int, index: int, res: List[int], info: List[int], fn: Callable[[List[int], int], None]
+) -> None:
+    """
+    i 번째 과녁에서 라이언이 승리하였는가?
+
+    arrow: 남은 화살의 개수
+    info: 어피치의 사격성적
+
+    O(2 ** 11) 의 시간복잡도를 가짐
+    """
+    N = len(res)
+    if arrow < 0:
+        # 불가능
+        return
+    if index == N:
+        # 화살 개수가 남아있을 수 있음!
+        fn(res, arrow)
+        return
+
+    # 실패
+    dfs2(arrow, index + 1, res, info, fn)
+
+    # 승리
+    amt = info[index] + 1
+    res[index] = amt
+    dfs2(arrow - amt, index + 1, res, info, fn)
+
+    res[index] = 0
+
+
 def compare(lhs: List[int], rhs: List[int]) -> int:
     """
     lhs - rhs의 결과를 리턴한다. 문제의 규칙에 따라 lhs가 라이언, rhs가 어피치
@@ -71,8 +102,36 @@ def solution(n, info: List[int]):
     return answer
 
 
+def solution2(n, info: List[int]):
+    maximum = 0
+    answer = [0] * 11
+
+    def each_case(ryan: List[int], arrow: int) -> None:
+        nonlocal answer, maximum
+
+        if arrow:
+            # 화살을 덜 쐈다.
+            ryan[-1] += arrow
+
+        # 화살을 다 쐈다.
+        compared = compare(ryan, info)
+        if maximum < compared:
+            maximum = compared
+            answer = ryan[:]
+        elif maximum == compared and less(answer, ryan):
+            answer = ryan[:]
+
+    dfs2(n, 0, [0 for _ in range(11)], info, each_case)
+
+    if maximum == 0:
+        return [-1]
+    return answer
+
+
 if __name__ == "__main__":
-    # print(compare([0, 2, 2, 0, 1, 0, 0, 0, 0, 0, 0], [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]))
+    solution = solution2
+    dfs2(3, 0, [0, 0, 0], [2, 1, 0], print)
     print(solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]))
     print(solution(1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
     print(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]))
+    print(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]))
