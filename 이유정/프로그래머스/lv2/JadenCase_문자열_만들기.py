553cd89bdc10448c0729703b79a2a30d643eda12
uuuuj <yjth4436@gmail.com>
2024-03-01 08:48:27 +0900

    [20240301]JadenCase 문자열 만들기

--- 이유정/프로그래머스/lv2/JadenCase_문자열_만들기.py
+++ 이유정/프로그래머스/lv2/JadenCase_문자열_만들기.py
@@ -1,35 +1,13 @@
-import string
-import sys
-
 
 def solution(s):
-    s_list = s.split()
-    # print(s_list)
-    if s_list[0][0] not in string.ascii_letters:
-        for i in range(1, len(s_list)):
-            if s_list[i][0] in string.ascii_lowercase:
-                fst = s_list[i][0]
-                fst_upper = fst.upper()
-                s_list[i] = s_list[i].replace(fst, fst_upper)
-            for check in s_list[i][1:]:
-                if check.isupper():
-                    check_lower = check.lower()
-                    s_list[i] = s_list[i].replace(check, check_lower)
-    else:
-        for i in range(0, len(s_list)):
-            if s_list[i][0] in string.ascii_lowercase:
-                fst = s_list[i][0]
-                fst_upper = fst.upper()
-                s_list[i] = s_list[i].replace(fst, fst_upper)
-
-            for check in s_list[i][1:]:
-                if check.isupper():
-                    check_lower = check.lower()
-                    s_list[i] = s_list[i].replace(check, check_lower)
-    answer = " ".join(s_list)
-    # answer = s_list[0][0]
+    answer = ''
+    s = s.split(' ')
+    for i in range(len(s)):
+        #captialize 내장함수를 사용하면 첫 문자가 알파벳일 경우 대문자로 만들고
+        #두번째 문자부터는 자동으로 소문자로 만든다
+        #첫 문자가 알파벳이 아니면 그대로 리턴한다
+        s[i] = s[i].capitalize()
+    answer = ' '.join(s)
     return answer
 
-input = sys.stdin.readline()
-result = solution(input)
-print(result)
+d
