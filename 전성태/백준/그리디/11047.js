d36a406034e74cc3b8e96d76ad8bcfb02417bc44
poqop721 <poqop721@naver.com>
2024-04-06 20:13:54 +0900

    [백준-11047] 동전 0

--- 전성태/백준/그리디/11047.js
+++ 전성태/백준/그리디/11047.js
@@ -1,3 +1,5 @@
+// 내 풀이
+
 const stdin = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
 let [N, K] = stdin[0].split(' ').map(Number)
 let A = []
@@ -23,3 +25,25 @@ while(true){
 }
 
 console.log(count)
+
+
+// 다른 사람 풀이
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [n, ...arr] = input;
+let [num, price] = n.split(' ');
+price = Number(price);
+arr = arr.map(i => Number(i));
+solution(n, price, arr);
+
+function solution(n, price, arr){
+    let result = 0;
+    for(let i=arr.length-1 ; i >= 0 ; i--){
+        if(price - arr[i] >= 0){
+            result += Math.floor(price/arr[i]);
+            price = price%arr[i];
+        }
+    }
+    console.log(result);
+}
